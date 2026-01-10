312c210c6ab28008b5bae8cacec014f91cdfafd8
Tabitha Hanegan <tabitha@tabithahanegan.com>
2026-01-10 18:05:14 -0500

    1

--- src/patch.bat
+++ src/patch.bat
@@ -1,7 +1,7 @@
 @echo off
 title Mario Party 6 - DX Patcher
 
-set VERSION=26.01.10
+set VERSION=26.01.10v2
 
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 echo Mario Party 6 - DX Patcher!
