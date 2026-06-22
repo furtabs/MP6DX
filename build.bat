67b0611caa9e273ddaa81c1a35db81466f2d27e4
tabitha hanegan <tabitha@tabithahanegan.com>
2026-06-21 21:41:30 -0400

    2606.21

--- build.bat
+++ build.bat
@@ -17,6 +17,23 @@ mkdir dist\store
 "compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Bowser Orb/Bowser Orb Rewrite.asm" && python "compiler/gecko.py" a.out 801BE730 tmp/bowser_orb_rewrite.txt1
 "compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Bowser Orb/Bowser Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249B64 tmp/bowser_orb_pointer.txt1 -ow
 
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Chance Orb/Chance Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249B3C tmp/chance_orb_pointer.txt1 -ow
+
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Chomp Call Orb/Chomp Call Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249CA4 tmp/chomp_call_orb_pointer.txt1 -ow
+
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/DK Orb/DK Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249B8C tmp/dk_orb_pointer.txt1 -ow
+
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Duel Orb/Duel Orb Fixer.asm" && python "compiler/gecko.py" a.out 801B9C60 tmp/duel_orb_fixer.txt1
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Duel Orb/Duel Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249B14 tmp/duel_orb_pointer.txt1 -ow
+
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Pink Boo Orb/Pink Boo Day 1.asm" && python "compiler/gecko.py" a.out 801B1BB4 tmp/pink_boo_orb_day_1.txt1
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Pink Boo Orb/Pink Boo Day 2.asm" && python "compiler/gecko.py" a.out 801B1EF8 tmp/pink_boo_orb_day_2.txt1
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Pink Boo Orb/Pink Boo Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249BDC tmp/pink_boo_orb_pointer.txt1 -ow
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Pink Boo Orb/Pink Boo Orb Pos Fixer.asm" && python "compiler/gecko.py" a.out 801B1C60 tmp/pink_boo_orb_pos_fixer.txt1
+
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Poison Shroom Orb/Poison Shroom Orb Type.asm" && python "compiler/gecko.py" a.out 802495DB tmp/poison_shroom_orb_type.txt1 -ow
+"compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Poison Shroom Orb/Poison Shroom Orb Type 2.asm" && python "compiler/gecko.py" a.out 802495E2 tmp/poison_shroom_orb_type_2.txt1 -ow
+
 "compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Soluna Orb/Soluna Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249C7C tmp/soluna_orb_pointer.txt1 -ow
 
 "compiler/codewrite/powerpc-gekko-as.exe" -a32 -mbig -mregnames -mgekko "src/asm/Wacky Watch Orb/Wacky Watch Orb Pointer.asm" && python "compiler/gecko.py" a.out 80249CCC tmp/wacky_watch_orb_pointer.txt1 -ow
